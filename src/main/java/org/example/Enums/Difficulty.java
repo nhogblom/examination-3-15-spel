abe445045100ed69b7c912dfca3ab3a369a98116
ivbe0111 <berezkinivan94@gmail.com>
2025-10-31 10:11:57 +0100

    Added Difficulty ENUM

---
+++ src/main/java/org/example/Enums/Difficulty.java
@@ -0,0 +1,30 @@
+package org.example.Enums;
+
+public enum Difficulty {
+
+    EASY("Easy",2,2),
+    MEDIUM("Medium",3,3),
+    HARD("Hard",4,4);
+
+    private final String description;
+    private final int xRows;
+    private final int yRows;
+
+    Difficulty(String description, int xRows, int yRows) {
+        this.description = description;
+        this.xRows = xRows;
+        this.yRows = yRows;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getxRows() {
+        return xRows;
+    }
+
+    public int getyRows() {
+        return yRows;
+    }
+}
