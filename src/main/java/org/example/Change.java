a6cfb75fce1fc875723f45749f8cea8a7cc2bbed
Niklas Högblom <nhogblom@gmail.com>
2025-11-03 15:16:52 +0100

    removed setters

--- src/main/java/org/example/Change.java
+++ src/main/java/org/example/Change.java
@@ -12,15 +12,8 @@ public class Change {
         return from;
     }
 
-    public void setFrom(int from) {
-        this.from = from;
-    }
-
     public int getTo() {
         return to;
     }
 
-    public void setTo(int to) {
-        this.to = to;
-    }
 }
